c60e8d2b0d258804c76de277656bf1d319f53edd
Federico Poloni <979716+fph@users.noreply.github.com>
2011-09-01 15:14:57 +0200

    stub symBasis

---
+++ symBasisHeuristic.m
@@ -0,0 +1,23 @@
+function v=symBasisHeuristic(U,varargin)
+% find a good starting row swap v for a symplectic basis
+% namely, the top half of Pi_v^T(U) shouldn't be too ill-conditioned
+%
+% v=symBasisHeuristic(U)
+%
+
+n=size(U,2);
+if(size(U,1)~=2*n)
+    error('cbrpack:oddSize','the input matrix must have an even number of rows');
+end
+
+[Q,R,E]=qr(U',0);
+d=matgic.diagm(R);
+
+if min(d)/max(d)<sqrt(eps(class(R)))
+    warning('cbrpack:illConditionedSubspace','the provided subspace basis is ill-conditioned --- don''t expect much from the results');
+end
+
+invE=1:length(E);
+invE(E)=invE;
+
+v=(invE(1:n)>invE(n+1:2*n));
--- testSymBasis.m
+++ testSymBasis.m
@@ -12,3 +12,10 @@ assertEqual(rowSwap(U,[1 1 1],'N'),[4 5 6 -1 -2 -3]');
 assertEqual(rowSwap(U,[1 0 0],'N'),[4 2 3 -1 5 6]');
 assertEqual(rowSwap(U,[1 0 0],'T'),[-4 2 3 1 5 6]');
 
+U=[2;1];
+[X,v]=symplecticSubspace2SymBasis(U);
+
+assertExceptionThrown(@() symplecticSubspace2SymBasis([1]),'cbrpack:oddSize');
+
+
+
