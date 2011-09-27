be866b56c83af1b695331d01ae4d650c3f350922
Federico Poloni <979716+fph@users.noreply.github.com>
2011-09-27 14:59:33 +0200

    add test

--- testUpdateCanBasis.m
+++ testUpdateCanBasis.m
@@ -15,3 +15,18 @@ assertEqual(pn,[6 4 1 9 8 2 3 5 10 7]);
 
 U2=canBasis2Subspace(Xn,pn);
 assertElementsAlmostEqual(subspace(U2,U),0);
+
+U=[111 12; 113 14; 115 116; 17 118; 119 110]; %for this matrix, the QRP heuristic leads to a max(max(abs(X)))>1
+[X,p]=subspace2CanBasis(U,'threshold',10);
+x=max(max(abs(X)));
+assert(x<10);
+assert(x>1.1);
+
+[X,p]=subspace2CanBasis(U,'threshold',1.2);
+x=max(max(abs(X)));
+assert(x<1.2);
+assert(x>1.1);
+
+[X,p]=subspace2CanBasis(U,'threshold',1.1);
+x=max(max(abs(X)));
+assert(x<1.1);
