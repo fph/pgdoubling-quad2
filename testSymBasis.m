91313dc5b0bb738682678a4374fecf04126bcb88
Federico Poloni <f.poloni@sns.it>
2011-12-05 13:47:17 +0100

    updateSymBasis

--- testSymBasis.m
+++ testSymBasis.m
@@ -21,9 +21,22 @@ assertExceptionThrown(@() symplecticSubspace2SymBasis([1]),'cbrpack:oddSize');
 U=randn(10,4);
 assertExceptionThrown(@() symplecticSubspace2SymBasis(U),'cbrpack:oddSize');
 
-U=randn(10,5).*exp(randn(10,5));
+n=8;
+for k=1:10
+    U=randomLagrangianSubspace(2*n);
+    v1=randi([0,1],n,1);
+    v2=randi([0,1],n,1);
+    [X1,v]=symplecticSubspace2SymBasis(U,'diagonalThreshold',inf,'initialRowSwap',v1);
+    assertEqual(v,v1);
+    [X2,v]=symplecticSubspace2SymBasis(U,'diagonalThreshold',inf,'initialRowSwap',v2);
+    assertEqual(v,v2);
+    range=[1:n];inout=range(v1~=v2);
+    [X,v]=updateSymBasis(X1,v1,inout);
+    assertEqual(v,v2);
+    assertElementsAlmostEqual(X,X2);
+end
+
+U=randomLagrangianSubspace(2*n);
 [X,v]=symplecticSubspace2SymBasis(U);
 U2=symBasis2SymplecticSubspace(X,v);
 assertElementsAlmostEqual(subspace(U2,U),0);
-
-
