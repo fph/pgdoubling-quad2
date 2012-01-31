9a58dd5f67271c4870c436754325ba81095af3f0
Federico Poloni <979716+fph@users.noreply.github.com>
2012-01-31 13:20:36 +0100

    better tests

--- testDoublingStep.m
+++ testDoublingStep.m
@@ -1,11 +1,16 @@
 function testDoublingStep
 
-n=10;
-X=randn(n);X=(X+X')/2;
-v=logical(randi(2,n,1)-1);
-[L,U]=symBasis2SymplecticPencil(X,v);
-[X2,v2]=doublingStep(X,v,[]);
-M=U\L;
-[XX,vv]=symplecticPencil2SymBasis(M^2,eye(n));
+reset(RandStream.getDefaultStream);
 
-assertElementsAlmostEqual(subspace(symBasis2SymplecticSubspace(X2,v2),symBasis2SymplecticSubspace(XX,vv)),0);
+n=10;
+
+for tries=1:10
+    X=randn(n);X=(X+X')/2;
+    v=logical(randi(2,n,1)-1);
+    [L,U]=symBasis2SymplecticPencil(X,v);
+    [X2,v2]=doublingStep(X,v,[]);
+    M=U\L;
+    [XX,vv]=symplecticPencil2SymBasis(M^2,eye(n));
+    
+    assertElementsAlmostEqual(subspace(symBasis2SymplecticSubspace(X2,v2),symBasis2SymplecticSubspace(XX,vv)),0);
+end
