bdc9c119b41bc3fdf63a1b4522c147a18bd9b1f2
Federico Poloni <979716+fph@users.noreply.github.com>
2012-01-31 13:19:11 +0100

    doubling step + testing

---
+++ testDoublingStep.m
@@ -0,0 +1,11 @@
+function testDoublingStep
+
+n=10;
+X=randn(n);X=(X+X')/2;
+v=logical(randi(2,n,1)-1);
+[L,U]=symBasis2SymplecticPencil(X,v);
+[X2,v2]=doublingStep(X,v,[]);
+M=U\L;
+[XX,vv]=symplecticPencil2SymBasis(M^2,eye(n));
+
+subspace(symBasis2SymplecticSubspace(X2,v2),symBasis2SymplecticSubspace(XX,vv))
