7e19df481b8e820ed36065a653ddaccf5b20df1a
Federico Poloni <979716+fph@users.noreply.github.com>
2012-02-16 13:47:28 +0100

    updating bases

---
+++ canBasis2canBasis.m
@@ -0,0 +1,9 @@
+function [X,targetp]=canBasis2canBasis(X,p,targetp)
+% updates a canbasis to a new specified "target" p
+%
+% [X,targetp]=canBasis2canBasis(X,p,targetp)
+%
+% equivalent to subspace2CanBasis(canBasis2Subspace(X,p),targetp);
+
+%crappy implementation for now...
+[X, targetp]=subspace2CanBasis(canBasis2Subspace(X,p),targetp);
