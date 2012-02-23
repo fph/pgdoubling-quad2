7588a859948598ef0c58b0c9ee430e6a9807a7b8
Federico Poloni <979716+fph@users.noreply.github.com>
2012-02-23 09:52:21 +0100

    refactored solveCARE

--- solveCARE.m
+++ solveCARE.m
@@ -4,12 +4,8 @@ function [X,Y,U,V]=solveCARE(A,G,Q,varargin)
 % [X,Y,U,V]=solveCARE(A,G,Q,...)
 %
 % options:
-% maxSteps: maximum number of step before returning (default:200)
-% tolerance: tolerance at which to stop, if attainable (default: eps)
-%   (the algorithm will stop anyway if the results aren't improving anymore)
-%
 % gamma: constant to use for the Cayley transform
-% verbose: be verbose
+% maxSteps, tolerance, verbose: as in doubling.m
 
 o=matgic.Options(varargin{:});
 
@@ -18,41 +14,7 @@ gamma=o.get('gamma',norm(H));
 if not(gamma>0)
     error 'gamma must be positive'
 end
-maxSteps=o.get('maxSteps',inf);
-tol=o.get('tolerance',eps);
-
-verbose=logical(o.get('verbose',false));
-
 [S,v]=symplecticPencil2SymBasis(H+gamma*eye(size(H)),H-gamma*eye(size(H)));
+[S,v]=optimizeSymBasis(S,v);
 
-steps=0;
-
-convergenceHistory=[nan nan nan nan nan nan];
-
-w=[]; %permutation guess
-while(true)
-    steps=steps+1;
-    [S,v,w,swaps1,swaps2,nGH,nEF]=doublingStep(S,v,w);
-    if(verbose)
-        fprintf('Step %3d, subspace change measure %5.2e, pseudo-residual measure %5.2e, swaps 2*%d+%d\n',steps,nGH,nEF,swaps1,swaps2);
-    end
-    if nEF<=tol
-        break;
-    end
-    convergenceHistory=[convergenceHistory(2:end) nEF];
-    if nEF<1e-6 && nEF>=mean(convergenceHistory) %stagnation
-        break
-    end
-    if(steps>maxSteps)
-        break;
-    end
-end
-
-n=length(A);
-first=1:n;second=n+1:2*n;
-
-U=rowSwap([eye(n);-S(second,second);],v(second),'N');
-X=U(second,:)/U(first,:);
-
-V=rowSwap([-S(first,first);eye(n)],v(first),'T');
-Y=V(first,:)/V(second,:);
+[X,Y,U,V]=doubling(S,v,o);
