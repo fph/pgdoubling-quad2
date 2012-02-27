84a00e4a754eee96ac8a94d9c9c119a6135281d1
Federico Poloni <979716+fph@users.noreply.github.com>
2012-02-27 10:38:59 +0100

    computes eigenvalues of XH*XJ given representations

---
+++ testHInfinityEigenvalues.m
@@ -0,0 +1,27 @@
+function testHInfinityEigenvalues
+
+reset(RandStream.getGlobalStream);
+
+n=6;
+
+XH=randn(n);XH=XH+XH';
+XJ=randn(n);XJ=XJ+XJ';
+
+
+[XHP,vh]=symplecticSubspace2SymBasis([XH;eye(n)]);
+[XJP,vj]=symplecticSubspace2SymBasis([XJ;eye(n)]);
+
+[XHP,vh]=optimizeSymBasis(XHP,vh);
+[XJP,vj]=optimizeSymBasis(XJP,vj);
+
+UH=symBasis2SymplecticSubspace(XHP,vh);
+UJ=symBasis2SymplecticSubspace(XJP,vj);
+
+first=1:n;second=n+1:2*n;
+
+ZH=UH(first,:);
+YH=UH(second,:);
+
+lambda=hInfinityEigenvalues(UH,UJ);
+
+assertElementsAlmostEqual(sort(lambda),sort(eig(XH*XJ)));
