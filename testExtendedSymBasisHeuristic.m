49c75ec7bbd33de0376f223b2d939ffcea175729
Federico Poloni <f.poloni@sns.it>
2012-02-28 15:54:47 +0100

    tests both heuristics (the new one fails atm)

--- testExtendedSymBasisHeuristic.m
+++ testExtendedSymBasisHeuristic.m
@@ -12,16 +12,21 @@ for i=1:50
     U(1:2*n,:)=rowSwap(U(1:2*n,:),v,'N');
     
     vh=extendedSymBasisHeuristic(U);
+    vhp=extendedSymBasisHeuristicPaper(U);
     
     assertEqual(logical(vh),logical(v)');
+    assertEqual(logical(vhp),logical(v)');
     
     EE=[U(1:n,:)' zeros(n+m,m)];
     AA=[(jay(n)*U(n+1:2*n,:))' U(2*n+1:2*n+m,:)'];
 %    size(AA),size(EE),size(v)
     
     S=warning('off','cbrpack:notSymplectic');
-    [X,v,invcond]=evenPencil2SymBasis(AA,EE,n/2,m,v);
+    [X,v,invcond]=evenPencil2SymBasis(AA,EE,n/2,m,vh);
+    [Xp,vp,invcondp]=evenPencil2SymBasis(AA,EE,n/2,m,vhp);
     warning(S);
     
     assertElementsAlmostEqual(invcond,1);
+    assertElementsAlmostEqual(invcondp,1);
+    
 end
