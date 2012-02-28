b7e54b127c8a49131cb781c5f0b4b037c1514064
Federico Poloni <f.poloni@sns.it>
2012-02-28 15:19:06 +0100

    return invcond

--- extendedSymBasisHeuristic.m
+++ extendedSymBasisHeuristic.m
@@ -1,11 +1,11 @@
-function v=extendedSymBasisHeuristic(U)
+function [v invcond]=extendedSymBasisHeuristic(U)
 % computes a "reasonable" value of v for an extended Lagrangian subspace
 %
-% v=symBasisHeuristic(U)
+% [v invcond]=symBasisHeuristic(U)
 %
 % uses QRP (just a crappy heuristic)
 %
-% U is 2n+m x n+m, 1:n can be swapped with n+1:2*n
+% U is 2n+m x n+m
 
 [a b]=size(U);
 n=a-b;
@@ -14,7 +14,9 @@ m=b-n;
 [Q,R,E]=qr(U',0);
 d=abs(diag(R));
 
-if min(d)/max(d)<sqrt(eps(class(R)))
+invcond=min(d)/max(d);
+
+if invcond<sqrt(eps(class(R)))
     warning('cbrpack:illConditionedSubspace','the provided subspace basis is ill-conditioned --- don''t expect much from the results');
 end
 
