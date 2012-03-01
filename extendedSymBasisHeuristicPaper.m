243ca743cfa34b58ae6f775ee7312197f87cc19e
Federico Poloni <f.poloni@sns.it>
2012-03-01 16:47:22 +0100

    un-screwed up extended basis heuristic

--- extendedSymBasisHeuristicPaper.m
+++ extendedSymBasisHeuristicPaper.m
@@ -11,16 +11,16 @@ function [v invcond]=extendedSymBasisHeuristicPaper(U)
 n=a-b;
 m=b-n;
 
-% computes a "restricted" (QRP)' factorization
+% start reducing the last block to triangular form, then invokes pirq
+[Q,R]=qr(U(2*n+1:end,:)');
+d=abs(diag(R));
+if min(d)/max(d)<sqrt(eps(class(U)))
+    warning('cbrpack:illConditionedThirdBlock','the extended part of the "extended Lagrangian" subspace is almost singular --- you are about to invert an ill-conditioned matrix');
+end
 
-[Pi R Q invcond]=pirq(U);
+U=U*Q; %now U3 should be block lower triangular
+[v,R2,Q2,invcond]=pirq(U(1:2*n,m+1:end));
 
-invPi=1:length(Pi);
-invPi(Pi)=invPi;
-%invPi=Pi;
-
-v=(invPi(1:n)>invPi(n+1:2*n));
-
-if invcond<sqrt(eps(class(R)))
+if invcond<sqrt(eps(class(U)))
     warning('cbrpack:illConditionedSubspace','the provided subspace basis is ill-conditioned --- don''t expect much from the results');
 end
