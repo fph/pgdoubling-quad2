6a0e0f852b9d7f41a9e5b2d962d71e909c98db8b
Federico Poloni <979716+fph@users.noreply.github.com>
2012-03-01 16:28:49 +0100

    modified pirq to return a pirq factorization of a symplectic matrix

--- testPirq.m
+++ testPirq.m
@@ -3,13 +3,12 @@ function testPirq
 reset(RandStream.getGlobalStream);
 
 n=4;
-m=2;
 
 for tries=1:100
-    U=randn(2*n+m,n+m);
-    [Pi,R,Q,invcond]=pirq(U);
+    U=randn(2*n,n);
+    [v,R,Q,invcond]=pirq(U);
     
-    assertVectorsAlmostEqual(abs(R*Q),abs(U(Pi,:)));
+    assertVectorsAlmostEqual(R*Q,rowSwap(U,v,'N'));
     assertVectorsAlmostEqual(Q*Q',eye(size(Q)));
-    assertVectorsAlmostEqual(abs(Pi(1:n)-Pi(n+1:2*n))/n,ones(n,1));
+    assert(invcond>1e-8);
 end
