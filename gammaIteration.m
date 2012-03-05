89e711fdb2b5ace4381d35fbf05352df31ac0e4b
Federico Poloni <979716+fph@users.noreply.github.com>
2012-03-05 18:22:06 +0100

    gammaIteration win

--- gammaIteration.m
+++ gammaIteration.m
@@ -19,34 +19,44 @@ upperBound=inf;
 gamma=norm([A1 B1 B2; C1 D11 D12; C2 D21 D22]); %half-assed starting value
 reason='';
 
-%S=warning('off','cbrpack:illConditionedMatrix');
+S=warning('off','cbrpack:illConditionedMatrix');
 %onCleanup(@() warning(S));
-%T=warning('off','cbrpack:illConditionedMatrix');
-%onCleanup(@() warning(T));
+
+upperF=nan; %we want to use the secant method, therefore we need to keep track of the "y"s in the points upperBound and lowerBound
+lowerF=nan; 
+
+iterations=0;
 
 while(upperBound-lowerBound>tol)
+    iterations=iterations+1;
     %gets the next value of gamma to try
     if upperBound==inf
         gamma=2*gamma;
+    elseif not(isnan(lowerF+upperF))
+        %TODO: tried this secant method but it is slower than bisection?
+%        gamma=lowerBound-lowerF*(upperBound-lowerBound)/(upperF-lowerF);
+        gamma=(upperBound+lowerBound)/2;
+        assert(gamma>lowerBound && gamma<upperBound);
     else
-        %TODO: secant method!
         gamma=(upperBound+lowerBound)/2;
     end
     
     if isempty(reason)
         %we are at the first iteration
+        %TODO: print some pretty header
     else
         %completing line printed at the previous iteration
         fprintf('result: %s\n',reason);
     end
         
-    fprintf('lower bound: %e, upper bound:%e, gamma tested=%e ',lowerBound,upperBound,gamma);
+    fprintf('It %3d yLower=%e, yUpper=%e, xLower: %e, xUpper:%e, gamma tested=%e ',iterations,lowerF,upperF,lowerBound,upperBound,gamma);
     
     [A,B,Q,R,S]=hInfinityControlPencil('J',gamma,A1,B1,B2,C1,C2,D11,D12,D21);
     [XJ YJ UJ VJ]=solveEcareSign(A,B,Q,R,S,'safer',true,'maxSteps',100);
     k=checkECAREInvariantSubspaceResidual(A,B,Q,R,S,UJ);
     if not(k.isGood)
         lowerBound=gamma;
+        lowerF=nan;
         reason='Could not solve the Riccati equation for X_J';
     end
     
@@ -55,10 +65,10 @@ while(upperBound-lowerBound>tol)
     k=checkECAREInvariantSubspaceResidual(A,B,Q,R,S,UH);
     if not(k.isGood)
         lowerBound=gamma;
+        lowerF=nan;
         reason='Could not solve the Riccati equation for X_H';
     end
 
-    
     nj=length(UJ)/2;
     nh=length(UH)/2;
     kj=rank(UJ(nj+1:end,:)'*UJ(1:nj,:),tol);
@@ -69,11 +79,15 @@ while(upperBound-lowerBound>tol)
     eigY=eigY(permutation);
     expectedZeros=nj+nh-kj-kh;
     assertElementsAlmostEqual(eigY(1:expectedZeros),zeros(expectedZeros,1));
+    fValue=eigY(expectedZeros+1);
     if all(eigY(expectedZeros+1:end)>0)  
         upperBound=gamma;
+        upperF=fValue;
         reason='Stabilizable';
     else
         lowerBound=gamma;
+        negativeIndex=find(eigY(expectedZeros+1:end)<0,1,'first');
+        lowerF=eigY(expectedZeros+negativeIndex);
         reason='Not stabilizable';
     end
 end
