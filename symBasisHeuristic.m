1e7c2ac952713ee573d0ca3f9ec180db8df61f98
Federico Poloni <f.poloni@sns.it>
2012-03-13 14:05:20 +0100

    added test to try heuristic sym basis

--- symBasisHeuristic.m
+++ symBasisHeuristic.m
@@ -1,11 +1,17 @@
-function [v invcond]=symBasisHeuristic(U)
+function [v invcond X]=symBasisHeuristic(U)
 % find a good starting row swap v for a symplectic basis
 % namely, the top half of Pi_v^T(U) shouldn't be too ill-conditioned
 %
-% [v invcond]=symBasisHeuristic(U)
+% [v invcond X]=symBasisHeuristic(U)
 %
 
-[v,R,q,invcond]=pirq(U);
+[v,R,Q,invcond]=pirq(U);
+
+n=length(U);
+
+if nargout>2
+    [X invcond]=rightLinSolve(R(n+1:end,:),R(1:n,:));
+end
 
 if invcond<sqrt(eps(class(R)))
     warning('cbrpack:illConditionedSubspace','the provided subspace basis is ill-conditioned --- don''t expect much from the results');
