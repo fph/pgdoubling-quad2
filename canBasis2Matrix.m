6858edc7b5b07ce6f9e7c6c2d4ade7302da647b3
Federico Poloni <979716+fph@users.noreply.github.com>
2012-06-26 14:48:08 +0200

    matrix2CanBasis + tests

--- canBasis2Matrix.m
+++ canBasis2Matrix.m
@@ -11,9 +11,5 @@ function X=canBasis2Matrix(X,p);
 % in fact, the matrix representation is a particular canBasis, so we just
 % have to get it
 
-n=size(X,2);
-in=find(p(n+1:end)<=n);
-out=find(p(1:n)>n);
-[X,p]=updateCanBasis(X,p,in,out);
-X(p(n+1:end)-n,p(1:n))=X;
-X=X';
+[m n]=size(X);
+X=canBasis2CanBasis(X,p,1:m+n);
---
+++ matrix2CanBasis.m
@@ -0,0 +1,28 @@
+function [X,p,invcond,swaps]=matrix2CanBasis(X,varargin)
+% Bounded canonical basis representation of a matrix
+%
+% [X,p,invcond]=subspace2CanBasis(M,options);
+%
+% input: M matrix, p initial permutation guess (may be empty, in this
+% case the procedure will use a O(n^3) heuristic based on QRP)
+%
+% output:
+% X,p: such that U(p(1:end),:) and [I;X] span the same subspace, where U=[I;M]
+%
+% Options: see subspace2CanBasis: 'permutation', 'threshold', 'maxswaps'
+%
+% (c) 2011-2012 F. Poloni <poloni@math.tu-berlin.de> and others 
+% see AUTHORS.txt and COPYING.txt for details
+% https://bitbucket.org/fph/pgdoubling
+
+o=Options(varargin{:});
+
+[m n]=size(X);
+p=1:m+n;
+
+if o.isSet('permutation')
+    [X p invcond]=canBasis2CanBasis(X,p,o.get('permutation'));
+    swaps=0;
+else
+    [X p invcond swaps]=optimizeCanBasis(X,p,o.get('threshold',[]),o.get('maxswaps',[]));
+end
