94ef08870e481168b09690fb6e17ec6f8fd1d305
Federico Poloni <979716+fph@users.noreply.github.com>
2012-07-09 18:03:22 +0200

    changed format of product with sparse L

--- productWithSparseL.m
+++ productWithSparseL.m
@@ -1,14 +1,32 @@
-function [v,w]=productWithSparseL(X,p,v,w)
+function [v,w]=productWithSparseL(X,p,v,mode)
 % apply the permuted lower triangular matrix that turns a space into its canBasis
 %
-% [Lv,wL]=productWithSparseL(X,p,v,w)
+% [Lv,wL]=productWithSparseL(X,p,v,mode)
 %
 % a canBasis can be seen as a matrix L=[I 0; -X I]*Pi' that turns U into
-% [Y;0]. This function applies this matrix to the given matrices to get L*v and w*L.
+% [Y;0]. This function applies this matrix to the given matrix.
+%
+% if mode=='N', computes L*v
+% if mode=='T', computes v*L
+% if mode=='I', computes L\v
+% if mode=='IT', computes v/L
+%
 
 [m n]=size(X);
-v=v(p,:);
-v(n+1:end,:)=v(n+1:end,:)-X*v(1:n,:);
 
-w(:,1:n)=w(:,1:n)-w(:,n+1:end)*X;
-w(:,p)=w;
+switch mode
+    case 'N'
+        v=v(p,:);
+        v(n+1:end,:)=v(n+1:end,:)-X*v(1:n,:);
+    case 'T'
+        v(:,1:n)=v(:,1:n)-v(:,n+1:end)*X;
+        v(:,p)=v;
+    case 'I'
+        v(n+1:end,:)=v(n+1:end,:)+X*v(1:n,:);
+        v(p,:)=v;
+    case {'IT','TI'}
+        v=v(:,p);
+        v(:,1:n)=v(:,1:n)+v(:,n+1:end)*X;
+    otherwise
+        error 'Wrong mode'
+end
