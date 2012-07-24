b619bc19e87c5e523819560f926697130c6473fa
Federico Poloni <f.poloni@sns.it>
2012-07-24 10:19:11 +0200

    turn a canBasis into a wuv

---
+++ testCanBasis2wuv.m
@@ -0,0 +1,13 @@
+function testCanBasis2wuv
+
+reset(RandStream.getGlobalStream);
+
+n=5;m=16;
+
+for trie=1:100
+   X=randn(n,m);
+   p=randperm(n+m);
+   M=canBasis2Matrix(X,p);
+   [w u v]=canBasis2wuv(X,p);
+   assertElementsAlmostEqual(M,w'/(eye(m)-u*v'));
+end
