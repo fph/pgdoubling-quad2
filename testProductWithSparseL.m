5395f477b021792f1a13b6d8c8991f0029712003
Federico Poloni <979716+fph@users.noreply.github.com>
2014-12-06 22:15:51 +0100

    another test converted

--- testProductWithSparseL.m
+++ testProductWithSparseL.m
@@ -8,16 +8,16 @@ m=5;n=8;
 for tries=1:100
     U=randn(m+n,n);
     
-    [X,p]=subspace2CanBasis(U);
-    L=[eye(n) zeros(n,m); -X eye(m)];
-    L(:,p)=L;
+    can=canBasisFromSubspace(U);
+    L=[eye(n) zeros(n,m); -can.X eye(m)];
+    L(:,can.p)=L;
     tmp=L*U;
     assertVectorsAlmostEqual(tmp(n+1:end,:),zeros(m,n)); %checks that the constructed L works
     
     %multiplies with a random vector
     v=randn(m+n,2);
-    assertVectorsAlmostEqual(productWithSparseL(X,p,v,'N'),L*v);
-    assertVectorsAlmostEqual(productWithSparseL(X,p,v','T'),v'*L);
-    assertVectorsAlmostEqual(productWithSparseL(X,p,v,'I'),L\v);
-    assertVectorsAlmostEqual(productWithSparseL(X,p,v','IT'),v'/L);
+    assertVectorsAlmostEqual(productWithSparseL(can,v,'N'),L*v);
+    assertVectorsAlmostEqual(productWithSparseL(can,v','T'),v'*L);
+    assertVectorsAlmostEqual(productWithSparseL(can,v,'I'),L\v);
+    assertVectorsAlmostEqual(productWithSparseL(can,v','IT'),v'/L);
 end
