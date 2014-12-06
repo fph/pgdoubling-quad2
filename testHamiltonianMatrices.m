cd2382754ce23f9576aa7b6fc8a377e8069c3d08
Federico Poloni <979716+fph@users.noreply.github.com>
2014-12-06 22:28:00 +0100

    fixed another test with Ham matrices

--- testHamiltonianMatrices.m
+++ testHamiltonianMatrices.m
@@ -5,18 +5,19 @@ n=6;
 J=jay(n);
 
 for tries=1:100
-    X=randn(n);X=X+X';
-    v=logical(randi(2,n,1)-1);
-    [A,E]=symBasis2HamiltonianPencil(X,v);
+    sym.X=randn(n);sym.X=sym.X+sym.X';
+    sym.v=logical(randi(2,n,1)-1);
+    sym.origin='hamiltonianPencil';
+    [A,E]=hamiltonianPencilFromSymBasis(sym);
     %generates Hamiltonian matrices
     assertVectorsAlmostEqual(A*J*E'+E*J*A',zeros(n));
     %pencil->symbasis->pencil
-    v=logical(randi(2,n,1)-1);
-    [A,E]=symBasis2HamiltonianPencil(X,v);
+    sym.v=logical(randi(2,n,1)-1);
+    [A,E]=hamiltonianPencilFromSymBasis(sym);
     assertVectorsAlmostEqual(A*J*E'+E*J*A',zeros(n));    
     M=randn(n);
     assertVectorsAlmostEqual(M*A*J*E'*M'+M*E*J*A'*M',zeros(n));
-    [X,v]=hamiltonianPencil2SymBasis(M*A,M*E,'initialSwap',v);
-    [A2,E2]=symBasis2HamiltonianPencil(X,v);
+    sym=symBasisFromHamiltonianPencil(M*A,M*E,'initialSwap',sym.v);
+    [A2,E2]=hamiltonianPencilFromSymBasis(sym);
     assertElementsAlmostEqual(subspace([A';E'],[A2';E2']),0);
 end
