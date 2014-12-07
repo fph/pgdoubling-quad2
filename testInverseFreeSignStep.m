7aa26a6ef9acb6de27b167af64a9f7633176861c
Federico Poloni <979716+fph@users.noreply.github.com>
2014-12-07 22:29:43 +0100

    refactored inverseFreeSignStep

--- testInverseFreeSignStep.m
+++ testInverseFreeSignStep.m
@@ -1,12 +1,16 @@
 function testInverseFreeSignStep
-n=6;
-X=randn(n);X=X+X';v=logical(randi(2,n,1)-1);
-
-[A,E]=symBasis2HamiltonianPencil(X,v);
-H=inv(E)*A;
-Hnew=1/2*(H+inv(H));
-
-[Xnew,vnew,w,swaps1,swaps2,res]=inverseFreeSignStep(X,v);
-
-[Anew,Enew]=symBasis2HamiltonianPencil(Xnew,vnew);
-assertElementsAlmostEqual(0,subspace([Anew';Enew'],[Hnew';eye(size(Hnew))]));
+reset(RandStream.getGlobalStream)
+for trie = 1:100
+    n=6;
+    sym.X=randn(n);sym.X=sym.X+sym.X';sym.v=logical(randi(2,n,1)-1);
+    sym.origin='hamiltonianPencil';
+    
+    [A,E]=hamiltonianPencilFromSymBasis(sym);
+    H=E\A;
+    Hnew=1/2*(H+inv(H));
+    
+    [symnew,w,swaps1,swaps2,res]=inverseFreeSignStep(sym);
+    
+    [Anew,Enew]=hamiltonianPencilFromSymBasis(symnew);
+    assertElementsAlmostEqual(0,subspace([Anew';Enew'],[Hnew';eye(size(Hnew))]));
+end
