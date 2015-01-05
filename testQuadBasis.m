358436f78a92dbe84b493c91427bc1c10129d435
Federico Poloni <fpoloni@di.unipi.it>
2015-01-05 19:27:16 +0100

    starting stuff to update quad bases

--- testQuadBasis.m
+++ testQuadBasis.m
@@ -1,5 +1,6 @@
 function testQuadBasis()
 
+% test symBasisFromQuadBasis
 quad.X = [1 2 3 4; 5 6 7 8; 9 10 11 12; 13 14 15 16];
 quad.v = [true true true false];
 sym = symBasisFromQuadBasis(quad);
@@ -9,3 +10,8 @@ Y =[-107  -122  -137    13
     -137  -158  -179    15
       13    14    15   256];
 assertElementsAlmostEqual(sym.X,Y);
+
+quad.X, quad.v
+sym=symBasisFromQuadBasis(quad);sym.X
+[quad.X, quad.v] = updateQuadBasisOut(quad.X,quad.v,2);
+sym2=symBasisFromQuadBasis(quad);sym2.X
