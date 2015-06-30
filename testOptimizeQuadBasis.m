5df08a7cc37d2a38698c51a1c2d2b5cb96d974d5
Federico Poloni <fpoloni@di.unipi.it>
2015-06-30 11:22:52 +0200

    single-threshold optimizeQuadBasis

--- testOptimizeQuadBasis.m
+++ testOptimizeQuadBasis.m
@@ -2,19 +2,16 @@ function testOptimizeQuadBasis
 
 reset(RandStream.getGlobalStream);
 
-for dt = [2 10 20]
-    for odt = dt+[4 20 40]
-        for trie = 1:25
-            n=9;
-            quad.X = randn(n) .* exp(4*randn(n)); quad.X = quad.X + quad.X';
-            quad.v = randi(2,n,1)==1;
-            quadOpt = optimizeQuadBasis(quad, dt, odt);
-            U = symplecticSubspaceFromQuadBasis(quad);
-            U2 = symplecticSubspaceFromQuadBasis(quadOpt);
-            assertElementsAlmostEqual(subspace(U,U2),0);
-            sym = symBasisFromQuadBasis(quadOpt);
-            assertEqual(diag(sym.X)<dt,true(n,1));
-            assertEqual(sym.X-diag(diag(sym.X))<odt,true(n,n));
-        end
+for threshold = [1.1 2 10 20]
+    for trie = 1:25
+        n=15;
+        quad.X = randn(n) .* exp(4*randn(n));
+        quad.v = randi(2,n,1)==1;
+        quadOpt = optimizeQuadBasis(quad, threshold);
+        U = symplecticSubspaceFromQuadBasis(quad);
+        U2 = symplecticSubspaceFromQuadBasis(quadOpt);
+        assertElementsAlmostEqual(subspace(U,U2),0);
+        sym = symBasisFromQuadBasis(quadOpt);
+        assertEqual(sym.X<threshold,true(size(sym.X)));
     end
 end
