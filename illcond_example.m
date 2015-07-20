69d45bf732ed750a879029ff991029d879c586d4
Federico Poloni <fpoloni@di.unipi.it>
2015-07-20 13:50:10 +0200

    refined example

--- illcond_example.m
+++ illcond_example.m
@@ -1,4 +1,4 @@
-reset(RandStream.getGlobalStream);
+%reset(RandStream.getGlobalStream);
 n = 10;
 clear quad;
 quad.v = [true(1,n) false(1,n)];
@@ -7,28 +7,29 @@ W = [randn(n,n-1) 1e6*randn(n,1)]; quad.X(~quad.v,~quad.v) = W;
 A = 1e6*randn(n); quad.X(~quad.v,quad.v) = A;
 U = symplecticSubspaceFromQuadBasis(quad);
 
+condU = cond(U)
+
 quadOpt = optimizeQuadBasis(quad);
 Uopt = symplecticSubspaceFromQuadBasis(quadOpt);
 quad.v
 quadOpt.v
-subspace(U,Uopt)
+distUopt = subspace(U,Uopt)
 
 sym = symBasisFromQuadBasis(quad);
 symOpt = optimizeSymBasis(sym);
 UsymOpt = symplecticSubspaceFromSymBasis(symOpt);
-subspace(U,UsymOpt)
+distUsymOpt = subspace(U,UsymOpt)
 
 X = symmetricMatrixFromSymBasis(sym);
 Xopt = symmetricMatrixFromSymBasis(symBasisFromQuadBasis(quadOpt));
 XsymOpt = symmetricMatrixFromSymBasis(symOpt);
 
-cholX = chol(X);
-cholXopt = chol(Xopt);
-cholXsymOpt = chol(XsymOpt);
+%cholX = chol(X);
+%cholXopt = chol(Xopt);
+%cholXsymOpt = chol(XsymOpt);
 
-norm(X-Xopt)/norm(X)
-norm(X-XsymOpt)/norm(X)
+errXopt = norm(X-Xopt)/norm(X)
+errXsymOpt = norm(X-XsymOpt)/norm(X)
 
-
-norm(cholX-cholXopt)/norm(cholX)
-norm(cholX-cholXsymOpt)/norm(cholX)
+%norm(cholX-cholXopt)/norm(cholX)
+%norm(cholX-cholXsymOpt)/norm(cholX)
